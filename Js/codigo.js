f3a08aedc1b265b14125d76a82761bc7f83a5500
Manuel Abrante <racerf5@gmail.com>
2014-06-17 16:52:48 -0430

    Perfil - falta un calendario

--- Js/codigo.js
+++ Js/codigo.js
@@ -2,5 +2,23 @@ $(document).on("ready",inicio);
 
 function inicio ()
 {
-	
+	tabsPerfil();
+}
+
+
+function tabsPerfil(){
+	$('#tab1').on("click",function(){
+		$('#tab1').hover();
+		hideAndShow("tabs2","tabs1");
+	})
+	$('#tab2').on("click",function(){
+		$('#tab2').hover();
+		$('#calendario').css({"margin-top":0});
+		hideAndShow("tabs1","tabs2");
+	})
+}
+
+function hideAndShow (ocultar,mostrar) {
+	$('#'+ocultar).hide("slow");
+	$('#'+mostrar).show("slide");
 }

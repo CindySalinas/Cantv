e4a3b6afeda48c014a0db589afcdb20b29ab6525
Manuel Abrante <racerf5@gmail.com>
2014-06-22 20:17:03 -0430

    correcciones

--- Js/fallasActualesAd.js
+++ Js/fallasActualesAd.js
@@ -4,7 +4,6 @@ function inicio ()
 {
 	 cargarPerfil();
 	 doMap();
-	 agregarMarca();
 	 var map;
 }	
 
@@ -34,7 +33,7 @@ function cargarPerfil(){
 function doMap(){
 	//crea el mapa 
 	map = new GMaps({
-    	div: '.map',
+    	div: '#map',
     	lat:10.174862,
 		lng:-67.962385,
 		zoom:8
@@ -60,64 +59,3 @@ function doMap(){
 	})
 
 }
-function agregarMarca(){
-	map = new GMaps({
-    	div: '#map2',
-    	lat:10.174862,
-		lng:-67.962385,
-		zoom:8
-	});
-
-	/*GMaps.on('click', map.map, function(e) {
-    	var index = map.markers.length;
-   		var lats = e.latLng.lat();
-    	var lngs = e.latLng.lng();
-
-    	var template = $('#edit_marker_template').text();
-
-    	var content = template.replace(/{{index}}/g, index).replace(/{{lats}}/g, lats).replace(/{{lngs}}/g, lngs);
-   		
-   		/*$.getJSON(url,{nom:'prueba',dir:'morro 1',lat:lats,lng:lngs}).done(function(data){
-   			alert(data.mensaje);
-   		});
-	    map.addMarker({
-	      lat: lat,
-	      lng: lng,
-	      title: 'Marker #' + index,
-	      infoWindow: {
-	        content : content
-	      }
-	    });
-		console.log(lats,lngs);
- 	 });*/
-}
-/*
-
-function marcarMapa(){
-	var url = "http://127.0.0.1/Cantv/jsonCantv/guardarCentral.php?jsoncallback=?";
-
-	GMaps.on('click', map.map, function(e) {
-    	var index = map.markers.length;
-   		var lats = e.latLng.lat();
-    	var lngs = e.latLng.lng();
-
-    	var template = $('#edit_marker_template').text();
-
-    	var content = template.replace(/{{index}}/g, index).replace(/{{lats}}/g, lats).replace(/{{lngs}}/g, lngs);
-   		
-   		$.getJSON(url,{nom:'prueba',dir:'morro 1',lat:lats,lng:lngs}).done(function(data){
-   			alert(data.mensaje);
-   		});
-   		/*doMap();
-   		$.getJSON(url2)
-	    map.addMarker({
-	      lat: lat,
-	      lng: lng,
-	      title: 'Marker #' + index,
-	      infoWindow: {
-	        content : content
-	      }
-	    });
-		console.log(lats,lngs);
- 	 });
-}*/

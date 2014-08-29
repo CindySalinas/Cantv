4ae2bd07e2148602dadf9d9607b7fcff3a01f6b3
CindyS-5 <cindysalinas15@gmail.com>
2014-08-28 20:56:22 -0430

    Pagina clientes diseño y funcionalidad listo

--- Js/clientes.js
+++ Js/clientes.js
@@ -4,32 +4,40 @@ function inicio ()
 {
 	menuPrin();
 	$('#ingresarClientes').on('click',ingresarCliente);
+
 	$('#dibujoConsultar').on('click',consultarClientes);
-	$('#textoBusqueda1').on('change',consultarClientes);
-	$('#textoBusqueda2').on('change',consultarClientes2);
+
+	$('#dibujoConsultar2').on('click',consultarClientes2);
 	$('#modificarClientes').on('click',modificar);
 	$('#eliminarClientes').on('click',eliminar);
-	llenarClientes();
-	llenarClientes2();
+	$("#formIngresarClientesNew").hide();
 }
 
-
 function menuPrin(){
-	$('#consultarCliente , #consultarCliente2').on('click',function(){
+	$('#consultarClienteNewDesign , #consultarCliente2').on('click',function(){
 		hideAndShow('consClientes','ingClientes, #mdClientes,#contenedorMenuClientes');
-	})
-	$('#modificarCliente , #modificarCliente2').on('click',function(){
+	});
+	$('#modificarClienteNewDesign , #modificarCliente2').on('click',function(){
 		hideAndShow('mdClientes','consClientes,#ingClientes,#contenedorMenuClientes');
-	})
-	$('#ingresarCliente , #ingresarCliente2').on('click',function(){
+	});
+	$('#ingresarClienteNewDesign , #ingresarCliente2').on('click',function(){
 		hideAndShow('ingClientes','mdClientes, #consClientes,#contenedorMenuClientes');
-	})
+	});
 	$('.atras').on('click',function(){
 		hideAndShow('contenedorMenuClientes','consClientes, #ingClientes, #mdClientes');
 		resets();
-	})
+	});
+
+	$(".otroAtrasModificarCliente").on("click",ocult);
+
+	$('#consultarClienteNewDesign , #consultarCliente2').on('click',llenarClientes);
+	$('#modificarClienteNewDesign , #modificarCliente2').on('click',llenarClientes2);
 }
 
+function ocult()
+{
+	$("#formIngresarClientesNew").hide();
+}
 function hideAndShow (mostrar,ocultar) {
 	$('#'+ocultar).hide("slow");
 	$('#'+mostrar).show("slide");
@@ -37,6 +45,7 @@ function hideAndShow (mostrar,ocultar) {
 
 function resets(){
 	$('input:text').val("");
+	$('.contact-form-email-message').val("");
 }
 
 function ingresarCliente(){
@@ -70,7 +79,7 @@ function consultarClientes(){
 			});
 		}
 		else{
-			alert(data.mensaje);
+			
 		}
 	})
 }
@@ -92,22 +101,36 @@ function llenarClientes(){
 	})
 }
 function consultarClientes2(){
+	
+	$('#formIngresarClientesNew').hide("slide");
 	var tabla = $('#tablaConsultarDatos2');
 	var busc = $('#textoBusqueda2').val();
 	$('.newTr').remove();
 	var url = "http://127.0.0.1/Cantv/jsonCantv/buscarClientes.php?jsoncallback=?";
+	var contar=0;
 	$.getJSON(url,{buscar :busc}).done(function(data){
 		if(data.num !=0){
 			$.each(data,function(i,item){
-				tabla.css({'display':'inline-table','margin':'10px auto'});
-				tabla.append('<tr class="newTr"><td> <a href="#" id='+item.idCtl+'>'+item.nomCtl+' </a> </td> <td> <a id='+item.idCtl+'>'+item.dirCtl+'</a></td></tr>')
-				$('.newTr').on('click','a',function(data){
-					llenarMod($(this).attr('id'));
-				});
+				contar++;
+				if(contar%2==0)
+				{
+					tabla.css({'display':'inline-table','margin':'10px auto'});
+					tabla.append('<tr class="newTr"><td>'+item.nomCtl+'</td> <td> '+item.dirCtl+'</td> <td><a href="#" id='+item.idCtl+'><span class="spanEditarCliente"></span></a></td></tr>');
+					$('.newTr').on('click','a',function(data){
+						llenarMod($(this).attr('id'));
+					});
+				}
+				else
+				{
+					tabla.css({'display':'inline-table','margin':'10px auto'});
+					tabla.append('<tr class="newTr"><td>'+item.nomCtl+'</td> <td> '+item.dirCtl+'</td> <td><a href="#" id='+item.idCtl+'><span class="spanEditarCliente2"></span></a></td></tr>');
+					$('.newTr').on('click','a',function(data){
+						llenarMod($(this).attr('id'));
+					});
+				}
 			});
 		}
 		else{
-			alert(data.mensaje);
 		}
 	})
 }
@@ -116,14 +139,28 @@ function llenarClientes2(){
 	var busc = $('#textoBusqueda2').val();
 	$('.newTr').remove();
 	var url = "http://127.0.0.1/Cantv/jsonCantv/cargarClientes.php?jsoncallback=?";
+	var contar=0;
 	$.getJSON(url).done(function(data){
 		if(data.num !=0){
 			$.each(data,function(i,item){
-				tabla.css({'display':'inline-table','margin':'10px auto'});
-				tabla.append('<tr class="newTr"><td> <a href="#" id='+item.idCtl+'>'+item.nomCtl+' </a> </td> <td> <a id='+item.idCtl+'>'+item.dirCtl+'</a></td></tr>')
-				$('.newTr').on('click','a',function(data){
-					llenarMod($(this).attr('id'));
-				});
+				contar++;
+				if(contar%2==0)
+				{
+					tabla.css({'display':'inline-table','margin':'10px auto'});
+					tabla.append('<tr class="newTr"><td>'+item.nomCtl+'</td> <td> '+item.dirCtl+'</td> <td><a href="#" id='+item.idCtl+'><span class="spanEditarCliente"></span></a></td></tr>');
+					$('.newTr').on('click','a',function(data){
+						llenarMod($(this).attr('id'));
+					});
+				}
+				else
+				{
+					tabla.css({'display':'inline-table','margin':'10px auto'});
+					tabla.append('<tr class="newTr"><td>'+item.nomCtl+'</td> <td> '+item.dirCtl+'</td> <td><a href="#" id='+item.idCtl+'><span class="spanEditarCliente2"></span></a></td></tr>');
+					$('.newTr').on('click','a',function(data){
+						llenarMod($(this).attr('id'));
+					});
+				}
+				
 			});
 		}
 		else{
@@ -131,13 +168,16 @@ function llenarClientes2(){
 		}
 	})
 }
+
+var idParaModificarOBorrar;
+
 function llenarMod(id){
-	$('#formIngresarClientes2').css({'display':'inline-table','margin':'10px auto'});
+	$('#formIngresarClientesNew').show("slide");
 	var url = "http://127.0.0.1/Cantv/jsonCantv/buscarClientesId.php?jsoncallback=?";
 	$.getJSON(url,{id:id}).done(function(data){
 		if(data!=0){
 			$.each(data,function(i,item){
-				$('#idHide').val(id);
+				idParaModificarOBorrar=id;
 				$('#nomCl2').val(item.nomCtl);
 				$('#dirCl2').val(item.dirCtl);
 			})
@@ -150,26 +190,27 @@ function llenarMod(id){
 
 function modificar(){
 	var url = "http://127.0.0.1/Cantv/jsonCantv/modificarCliente.php?jsoncallback=?";
-	var id = $('#idHide').val();
+
+	var id = idParaModificarOBorrar;
 	var nom = $('#nomCl2').val();
 	var dir = $('#dirCl2').val();
-	var tabla = $('#tablaConsultarDatos2');
-	var form = $('#formIngresarClientes2');
-	$.getJSON(url,{nom:nom,dir:dir,id:id}).done(function(data){
-		alert(data.mensaje);
-		tabla.hide();
+	var form = $('#formIngresarClientesNew');
+
+	$.getJSON(url,{nom:nom,dir:dir,id:id}).done(function(data)
+	{
+		llenarClientes2();
 		form.hide();
 		resets();
-	})
+	});
 }
 function eliminar(){
 	var url = "http://127.0.0.1/Cantv/jsonCantv/eliminarCliente.php?jsoncallback=?";
-	var id = $('#idHide').val();
+	var id = idParaModificarOBorrar;
 	var tabla = $('#tablaConsultarDatos2');
-	var form = $('#formIngresarClientes2');
-	$.getJSON(url,{id:id}).done(function(data){
-		alert(data.mensaje);
-		tabla.hide();
+	var form = $('#formIngresarClientesNew');
+	$.getJSON(url,{id:id}).done(function(data)
+	{
+		llenarClientes2();
 		form.hide();
 		resets();
 	});
